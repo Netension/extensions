a45819b72bffeceea0f57bdc6a9ac57149d560fe
Bence Mihucza <Mihben@gmail.com>
2021-02-19 07:57:53 +0100

    Implement ServiceCollection extensions.

---
+++ src/Netension.Extensions.Correlation/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Netension.Extensions.Correlation
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegistrateCorrelation(this IServiceCollection services)
+        {
+            services.AddScoped<CorrelationContext>();
+            services.AddScoped<ICorrelationAccessor>((provider) => provider.GetRequiredService<CorrelationContext>());
+            services.AddScoped<ICorrelationMutator>((provider) => provider.GetRequiredService<CorrelationContext>());
+        }
+    }
+}
