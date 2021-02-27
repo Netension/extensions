1e260282643a19dbd4ebd4346f34ef30a37404e1
Bence Mihucza <Mihben@gmail.com>
2021-02-28 00:18:29 +0100

    Implement SecureString extensions.

---
+++ src/Netension.Extensions.Security/SecureStringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Netension.Extensions.Security
+{
+    public static class SecureStringExtensions
+    {
+        public static string Decrypt(this SecureString value)
+        {
+            var valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+
+        public static SecureString Encrypt(this string value)
+        {
+            var result = new SecureString(); 
+            result.Clear();
+
+            foreach (char c in value.ToCharArray())
+            {
+                result.AppendChar(c);
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Netension.Extensions.Security/SecureStringJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Netension.Extensions.Security
+{
+    public class SecureStringJsonConverter : JsonConverter<SecureString>
+    {
+        public override SecureString Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            if (string.IsNullOrEmpty(value)) return null;
+
+            return value.Encrypt();
+        }
+
+        public override void Write(Utf8JsonWriter writer, SecureString value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.Decrypt());
+        }
+    }
+}
