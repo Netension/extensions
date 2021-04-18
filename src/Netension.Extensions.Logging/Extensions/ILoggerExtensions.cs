decb78af56bcee813bca2d51b6d970578b7683f6
Bence, Mihucza <mihben@gmail.com>
2021-04-18 14:25:28 +0200

    Logger extension for log context

---
+++ src/Netension.Extensions.Logging/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Netension.Extensions.Logging.Extensions
+{
+    public static class ILoggerExtensions
+    {
+        public static IDisposable BeginScope(this ILogger logger, string key, object value)
+        {
+            return logger.BeginScope(new Dictionary<string, object> { [key] = value });
+        }
+    }
+}
