648a3d2af5696c4ad1a0d9b8913f6f5ed741cb87
Bence Mihcua <mihben@gmail.com>
2021-07-17 10:37:43 +0200

    Added HttpResponseExtension
    Implemented HttpResponse extensions:
    - WriteAsJsonAsync
    - OkAsJsonAsync
    - BadRequestAsJsonAsync
    - InternalServerErrorAsJsonAsync

---
+++ src/Netension.Extensions.Http/Extensions/HttpResponseExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mime;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class HttpResponseExtensions
+    {
+        public static async Task InternalServerErrorAsJsonAsync<TContent>(this HttpResponse response, TContent content, CancellationToken cancellationToken)
+        {
+            response.StatusCode = StatusCodes.Status500InternalServerError;
+            await response.WriteAsJsonAsync(content, cancellationToken).ConfigureAwait(false);
+        }
+
+        public static async Task BadRequestAsJsonAsync<TContent>(this HttpResponse response, TContent content, CancellationToken cancellationToken)
+        {
+            response.StatusCode = StatusCodes.Status400BadRequest;
+            await response.WriteAsJsonAsync(content, cancellationToken).ConfigureAwait(false);
+        }
+
+        public static async Task OkAsJsonAsync<TContent>(this HttpResponse response, TContent content, CancellationToken cancellationToken)
+        {
+            response.StatusCode = StatusCodes.Status200OK;
+            await response.WriteAsJsonAsync(content, cancellationToken).ConfigureAwait(false);
+        }
+
+        public static async Task WriteAsJsonAsync<TContent>(this HttpResponse response, TContent content, CancellationToken cancellationToken)
+        {
+            response.ContentType = MediaTypeNames.Application.Json;
+            await response.WriteAsync(JsonSerializer.Serialize(content), cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
