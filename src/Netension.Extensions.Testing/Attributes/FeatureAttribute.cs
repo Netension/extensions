2d07ce82aa9352c25a1541e5bc6b41ecb76d6f3e
Mihucza Bence <mihben@gmail.com>
2021-07-17 12:45:28 +0200

    Added Feature attribute

---
+++ src/Netension.Extensions.Testing/Attributes/FeatureAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit.Sdk;
+
+namespace Xunit
+{
+    [TraitDiscoverer("Netension.Testing.FeatureTraitDiscoverer", "Netension.Extensions.Testing")]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class FeatureAttribute : Attribute, ITraitAttribute
+    {
+        public FeatureAttribute(string name) { }
+    }
+}
---
+++ src/Netension.Extensions.Testing/Discoverers/FeatureTraitDiscoverer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Netension.Testing
+{
+    public class FeatureTraitDiscoverer : ITraitDiscoverer
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            var ctorArgs = traitAttribute.GetConstructorArguments().Cast<string>().ToList();
+            yield return new KeyValuePair<string, string>(string.Empty, ctorArgs[0]);
+        }
+    }
+}
